0a747aea4fb79e6cf90a90ff2d90e69b3a524bfd
ddanforth <dorothy@danforthmedia.com>
2014-06-22 16:36:09 -0400

    Updating assingment R file with comments.

--- cachematrix.R
+++ cachematrix.R
@@ -2,19 +2,23 @@
 ## functions do
 
 ## Write a short comment describing this function
-## Creating a  matrix element, setting the inverse, saving it in cache
+## COMMENT: Creating a  matrix element, setting the inverse, saving it in cache
 
 makeCacheMatrix <- function(x = matrix()) {
   
   m <- NULL
+
   set <- function(y) {
     x <<- y
     m <<- NULL
   }
+
   get <- function() x
+
   setmatrix <- function(solve) m <<- solve
   getmatrix <- function() m
-      
+
+        
   list(set = set, get = get,
        setmatrix = setmatrix,
        getmatrix = getmatrix)
@@ -23,16 +27,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-## Checks to see if matrix in cache, if so retrieve, get and return inverse. 
+## COMMENT: Checks to see if matrix in cache, if so retrieve, get and return inverse. 
   
 cacheSolve <- function(x = matrix(), ...) {
         ## Return a matrix that is the inverse of 'x'
   
   m <- x$getmatrix()
+
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
   }
+
   matrix <- x$get()
   m <- solve(matrix, ...)
   x$setmatrix(m)
